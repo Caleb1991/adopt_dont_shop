90646c5935fd5d87463546cdee37f464fef20fdc
Caleb Wittman <calebjwittman1991@gmail.com>
2021-05-24 16:48:41 -0600

    Create search functionality that returns all pets with given name

--- spec/features/applications/show_spec.rb
+++ spec/features/applications/show_spec.rb
@@ -12,6 +12,9 @@ RSpec.describe 'Application Show Page' do
     @pet_2 = @shelter_1.pets.create!(adoptable: false, age: 7, breed: 'Bernese Mountain Dog', name: 'Watermelon')
     @pet_3 = @shelter_2.pets.create!(adoptable: true, age: 1, breed: 'Golden Retriever', name: 'Sir William Wallace')
     @pet_4 = @shelter_2.pets.create!(adoptable: true, age: 5, breed: 'Border Collie', name: 'Bobby Mounahan')
+    @pet_5 = @shelter_2.pets.create!(adoptable: true, age: 1, breed: 'Goldent Retriever', name: 'Huey')
+    @pet_6 = @shelter_2.pets.create!(adoptable: true, age: 3, breed: 'Goldent Retriever', name: 'Huey')
+    @pet_7 = @shelter_2.pets.create!(adoptable: true, age: 7, breed: 'Border Collie', name: 'Huey')
 
     @application_1 = Application.create!(name: 'Roald Marshallsen', state: 'Colorado', city: 'Arvada', address: '1744 N. Pole Ln.', zip_code: 80004, description: 'Im a cool guy', status: 'Pending')
     @application_2 = Application.create!(name: 'Matt Smith', state: 'Colorado', city: 'Westminster', address: '2314 Gamble Oak St.', zip_code: 80003, description: 'Because adopting is cool', status: 'In Progress')
@@ -23,7 +26,7 @@ RSpec.describe 'Application Show Page' do
     @pet_app_4 = PetApplication.create!(pet_id: @pet_4.id, application_id: @application_2.id)
   end
 
-  it 'Shows the only the given applications attributes' do
+  it 'Shows only the given applications attributes' do
     visit "/applications/#{@application_1.id}"
 
     expect(page).to have_content(@application_1.name)
@@ -46,5 +49,21 @@ RSpec.describe 'Application Show Page' do
     click_on "#{@pet_1.name}"
 
     expect(current_path).to eq("/pets/#{@pet_1.id}")
+  end
+
+  it 'has a form to search for a pets name' do
+    visit "/applications/#{@application_1.id}"
+
+    expect(page).to have_content('Pet Name')
+    expect(page).to have_field(:name)
+  end
+
+  it 'has a link to all pets with the given name' do
+    visit "/applications/#{@application_1.id}"
+
+    fill_in :name, with: 'Huey'
+
+    expect(page).to have_link(@pet_7.name)
+    expect(page).to_not have_link(@pet_4.name)
   end
 end
